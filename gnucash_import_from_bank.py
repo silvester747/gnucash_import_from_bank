4344e3166a25dbf59fbbe2baa0808272cef20baf
Rob van der Most <silvester747@gmail.com>
2018-04-08 16:46:01 +0200

    Use argparse for command-line options.

--- gnucash_import_from_bank.py
+++ gnucash_import_from_bank.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import argparse
 import locale
 import os
-import sys
 
 from gnucash_import_converter.gnucash import GnuCashCsvWriter
 from gnucash_import_converter.rabobank import RabobankCsvReader
 
-assert len(sys.argv) == 2
+parser = argparse.ArgumentParser(
+        description="Convert bank statements into format to import into GNUCash."
+        )
+parser.add_argument(
+        'input_file',
+        metavar='INPUT_FILE',
+        action='store',
+        type=str,
+        help='Transaction file to import.'
+        )
+parser.add_argument(
+        '-o', '--output_dir',
+        action='store',
+        default=os.getcwd(),
+        type=str,
+        required=False,
+        help='Directory to store files to import into GNUCash. Defaults to the current working directory.'
+        )
+args = parser.parse_args()
 
 locale.setlocale(locale.LC_ALL, "")
 
-with GnuCashCsvWriter(os.getcwd()) as gnucash_writer:
-    with RabobankCsvReader(sys.argv[1]) as rabo_reader:
+with GnuCashCsvWriter(args.output_dir) as gnucash_writer:
+    with RabobankCsvReader(args.input_file) as rabo_reader:
         for statement in rabo_reader:
             gnucash_writer.write_statement(statement)
 
--- test/test_e2e.py
+++ test/test_e2e.py
@@ -2,9 +2,10 @@ import os
 import subprocess
 import shutil
 
+
 def test_full_import(tmpdir):
     """
-    Test full script run.
+    Import a simple transactions file in Rabobank CSV format.
     """
     input_csv = os.path.join(os.path.dirname(__file__), "rabobank.csv")
     assert os.path.isfile(input_csv)
@@ -18,6 +19,31 @@ def test_full_import(tmpdir):
                            shell=True) == 0
 
     out_csv = tmpdir.join("NL12RABO1234567890.csv")
+    assert out_csv.check()
+
+    expected_content = """Date,Description,Notes,Account,Deposit,Withdrawal,Balance
+2018-02-28,Computershop (987654321),Nice computer stuff ,NL12RABO1234567890,0,"148,99","419,84"
+"""
+    assert out_csv.read_text("latin1") == expected_content
+
+
+def test_import_to_custom_output_directory(tmpdir):
+    input_csv = os.path.join(os.path.dirname(__file__), "rabobank.csv")
+    assert os.path.isfile(input_csv)
+    shutil.copy(input_csv, tmpdir)
+
+    script = os.path.join(os.path.dirname(__file__), "..", "gnucash_import_from_bank.py")
+    assert os.path.isfile(script)
+
+    output_dir = tmpdir.join('my_custom_output_dir')
+
+    assert subprocess.call("{} {} --output_dir {}".format(script,
+                                                          tmpdir.join("rabobank.csv"),
+                                                          output_dir),
+                           cwd=str(tmpdir),
+                           shell=True) == 0
+
+    out_csv = output_dir.join("NL12RABO1234567890.csv")
     assert out_csv.check()
 
     expected_content = """Date,Description,Notes,Account,Deposit,Withdrawal,Balance
